900fad9ab9d859d3c83235516b2eb3374ccdbb94
shalinkpatel <shalin_patel@brown.edu>
2020-07-31 00:03:29 -0400

    Added latexify

--- scripts/view_res.jl
+++ scripts/view_res.jl
@@ -1,6 +1,9 @@
 using DrWatson
 @quickactivate "GCN_HM_GRN-Integration"
 
-using DataFrames
+using DataFrames, Latexify
 df = collect_results(datadir("port_no_sep"))
-print(df[!, 1:4])
+df = df[!, 1:4]
+print(df)
+copy_to_clipboard(true)
+print(latexify(df; env=:table, latex=false))
