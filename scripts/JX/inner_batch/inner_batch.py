88d30c304d4f3cf3268f246b05863305c6c688a8
shalinkpatel <shalin_patel@brown.edu>
2020-08-01 14:25:17 -0400

    hidden size

--- scripts/JX/inner_batch/inner_batch.py
+++ scripts/JX/inner_batch/inner_batch.py
@@ -176,6 +176,6 @@ def run_sim(cl, batches, layer):
     train_mask = idx[:10000]
     test_mask = idx[10000:]
     
-    net = GCN(94, 1000, 750, 400, 50, 2, layer_dict[layer])
+    net = GCN(94, 900, 750, 400, 50, 2, layer_dict[layer])
     return train_model(net, train_loader, 1500, 0.0005, train_mask, test_mask, mask)
 
