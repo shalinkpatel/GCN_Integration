b7a5406581a3cdea76de6f51c60c7ff396325eaf
shalinkpatel <shalin_patel@brown.edu>
2020-08-01 11:01:14 -0400

    Produced summary plots

--- scripts/view_res.jl
+++ scripts/view_res.jl
@@ -1,8 +1,10 @@
 using DrWatson
 @quickactivate "GCN_HM_GRN-Integration"
 
+exp = "port_batched"
+
 using DataFrames, Latexify
-df = collect_results(datadir("large_batch"));
+df = collect_results(datadir(exp));
 
 using Plots
 theme(:juno)
@@ -12,10 +14,8 @@ plts = []
 for i ∈ 1:size(df, 1)
 	plt = plot([df.auc_list[i], df.losses_train[i], df.losses_test[i]],
 	        labels = ["auc" "train" "test"], legend = :right,
-	        xlabel = "Epochs", title = "Training for Cell Line $(df.cl[i]) with batches = $(df.batch[i]) and $(df.layer[i]) layers (Best AUC: $(DrWatson.roundval(df.auc[i]; digits = 4, scientific = 4)))", size = (800, 500), dpi = 100)
-	cl, layer, batches = df.cl[i], df.layer[i], df.batch[i]
-	d = @dict cl layer batches
+	        xlabel = "Epochs", title = "Training for Cell Line $(df.cl[i]) with lr = $(df.lr[i]) and $(df.layer[i]) layers (Best AUC: $(DrWatson.roundval(df.auc[i]; digits = 4, scientific = 4)))", size = (800, 500), dpi = 100)
 	push!(plts, plt)
 end
-p = plot(plts..., layout=(2, 2), size = (1600, 1000), dpi = 200)
-savefig(p, plotsdir("large_batch", "summary.png"))
+p = plot(plts..., layout=(3, 2), size = (1600, 1500), dpi = 200)
+savefig(p, plotsdir(exp, "summary.png"))
