b4029fdb66849a5024e3cc87d4dbdbf0ddf7cc7d
shalinkpatel <shalin_patel@brown.edu>
2020-08-05 12:30:08 -0400

    other issue

--- scripts/JX/inner_batch/inner_batch.sh
+++ scripts/JX/inner_batch/inner_batch.sh
@@ -3,10 +3,8 @@
 #SBATCH -c 4
 #SBATCH -p gpu --gres=gpu:1
 #SBATCH --time 30:00:00
-#SBATCH --mem-per-cpu 15G
+#SBATCH --mem-per-cpu 25G
 #SBATCH --job-name inner_batch
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=shalin_patel@brown.edu
 
 source ~/ml/bin/activate
 julia inner_batch.jl
