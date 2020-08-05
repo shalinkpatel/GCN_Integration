edcd11682c5f0a8e220b37539d865e1765d9fda3
shalinkpatel <shalin_patel@brown.edu>
2020-08-05 09:23:35 -0400

    changes

--- scripts/JX/large_batch/large_batch.sh
+++ scripts/JX/large_batch/large_batch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --nodes 1
-#SBATCH -c 4
+#SBATCH -c 2
 #SBATCH -p gpu --gres=gpu:1
 #SBATCH --time 30:00:00
-#SBATCH --mem-per-cpu 15G
+#SBATCH --mem-per-cpu 30G
 #SBATCH --job-name gcn
 
 source ~/ml/bin/activate
