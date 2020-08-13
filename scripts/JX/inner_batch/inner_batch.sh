628b3ce66eea967219c3e3865d50a63728d69ce9
shalinkpatel <shalin_patel@brown.edu>
2020-08-13 10:47:17 -0400

    test

--- scripts/JX/inner_batch/inner_batch.sh
+++ scripts/JX/inner_batch/inner_batch.sh
@@ -3,8 +3,7 @@
 #SBATCH -c 2
 #SBATCH -p gpu --gres=gpu:1
 #SBATCH --time 30:00:00
-#SBATCH --mem-per-cpu 10G
-#SBATCH --constraint=v100
+#SBATCH --mem-per-cpu 20G
 #SBATCH --job-name inner_batch
 
 source ~/ml/bin/activate
