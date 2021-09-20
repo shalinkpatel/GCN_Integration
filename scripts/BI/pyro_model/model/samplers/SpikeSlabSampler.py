7fd80f89ed21db8bcb267a118fac2d3059f1f07c
Shalin Patel <shalin.kp.patel@gmail.com>
2021-09-20 18:45:20 -0400

    Rudimentary Spike and Slab model. Fingers crossed this works.

--- scripts/BI/pyro_model/model/samplers/SpikeSlabSampler.py
+++ scripts/BI/pyro_model/model/samplers/SpikeSlabSampler.py
@@ -7,35 +7,52 @@ from .BaseSampler import BaseSampler
 
 
 class BetaBernoulliSampler(BaseSampler):
-    def __init__(self, name: str, alpha: float, beta: float):
+    def __init__(self, name: str, theta: float, alpha1: float, beta1: float, alpha2: float, beta2: float):
         super().__init__(name)
-        self.alpha = alpha
-        self.beta = beta
+        self.theta = theta
+        self.alpha1 = alpha1
+        self.beta1 = beta1
+        self.alpha2 = alpha2
+        self.beta2 = beta2
 
     def sample_model(self, X, y, explainer):
-        alpha = torch.tensor([self.alpha for _ in range(explainer.N)]).to(explainer.device)
-        beta = torch.tensor([self.beta for _ in range(explainer.N)]).to(explainer.device)
-        f = pyro.sample("f", dist.Beta(alpha, beta).to_event(1))
-        m = pyro.sample("m", dist.Bernoulli(f).to_event(1))
+        theta = torch.tensor([self.theta for _ in range(explainer.N)]).to(explainer.device)
+        t = pyro.sample("t", dist.Bernoulli(theta).to_event(1))
+
+        f1 = pyro.sample("f1", dist.Beta(self.alpha1, self.beta1).expand([explainer.N]).to_event(1))
+        f2 = pyro.sample("f2", dist.Beta(self.alpha2, self.beta2).expand([explainer.N]).to_event(1))
+
+        m = pyro.sample("m", dist.Bernoulli((1 - t) * f1 + t * f2).to_event(1))
         mean = explainer.model(X, explainer.edge_index_adj[:, m == 1])[explainer.mapping].reshape(-1)
         y_sample = pyro.sample("y_sample", dist.Categorical(y))
         _ = pyro.sample("y_hat", dist.Categorical(mean), obs=y_sample)
 
     def sample_guide(self, X, y, explainer):
-        alpha = torch.tensor([self.alpha for _ in range(explainer.N)]).to(explainer.device)
-        beta = torch.tensor([self.beta for _ in range(explainer.N)]).to(explainer.device)
-        alpha_q = pyro.param("alpha_q", alpha, constraint=constraints.positive)
-        beta_q = pyro.param("beta_q", beta, constraint=constraints.positive)
-        f = pyro.sample("f", dist.Beta(alpha_q, beta_q).to_event(1))
-        m = pyro.sample("m", dist.Bernoulli(f).to_event(1))
+        theta = torch.tensor([self.theta for _ in range(explainer.N)]).to(explainer.device)
+        t_q = pyro.param("t_q", theta, constraint=constraints.unit_interval)
+        t = pyro.sample("t", dist.Bernoulli(t_q).to_event(1))
+
+        alpha1_q = pyro.param("a1_q", self.alpha1, constraint=constraints.positive)
+        beta1_q = pyro.param("b1_q", self.beta1, constraint=constraints.positive)
+        f1 = pyro.sample("f1", dist.Beta(alpha1_q, beta1_q).expand([explainer.N]).to_event(1))
+
+        alpha2_q = pyro.param("a2_q", self.alpha2, constraint=constraints.positive)
+        beta2_q = pyro.param("b2_q", self.beta2, constraint=constraints.positive)
+        f2 = pyro.sample("f2", dist.Beta(alpha2_q, beta2_q).expand([explainer.N]).to_event(1))
+
+        m = pyro.sample("m", dist.Bernoulli((1 - t) * f1 + t * f2).to_event(1))
         mean = explainer.model(X, explainer.edge_index_adj[:, m == 1])[explainer.mapping].reshape(-1)
         y_sample = pyro.sample("y_sample", dist.Categorical(logits=y))
         _ = pyro.sample("y_hat", dist.Categorical(logits=mean), obs=y_sample)
 
     def edge_mask(self, explainer):
-        alpha = pyro.param('alpha_q')
-        beta = pyro.param('beta_q')
-        return dist.Beta(alpha, beta).mean
+        t = pyro.param("t_q")
+        alpha1 = pyro.param("a1_q")
+        beta1 = pyro.param("b1_q")
+        alpha2 = pyro.param("a2_q")
+        beta2 = pyro.param("b2_q")
+
+        return (1 - t) * dist.Beta(alpha1, beta1).mean + t * dist.Beta(alpha2, beta2).mean
 
     def loss_fn(self, model, guide, *args, **kwargs):
         return pyro.infer.Trace_ELBO().differentiable_loss(model, guide, *args)
