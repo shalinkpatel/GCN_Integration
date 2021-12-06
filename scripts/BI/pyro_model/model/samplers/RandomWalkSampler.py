c10e767a21c305f14acf2bafd0ceb7acebe98df8
shalinkpatel <shalin_patel@brown.edu>
2021-12-06 16:55:26 -0500

    added label tests

--- scripts/BI/pyro_model/model/samplers/RandomWalkSampler.py
+++ scripts/BI/pyro_model/model/samplers/RandomWalkSampler.py
@@ -103,10 +103,44 @@ class RandomWalkSampler(BaseSampler):
         _ = pyro.sample("y_hat", dist.Categorical(probs=mean/mean.sum()), obs=y_sample)
 
     def edge_mask(self, explainer):
-        m = torch.zeros([self.ne])
-        for i in range(self.ne):
-            m[i] = pyro.param(f"m_q_{i}")
-        return m
+        edge_mask = torch.zeros([self.ne])
+        sample_dists = list(map(lambda i: pyro.param(f"m_q_{i}"), range(self.ne)))
+        G = to_networkx(Data(edge_index=explainer.edge_index_adj, num_nodes=explainer.edge_index_adj.max()))
+        for _ in range(1000):
+            nodes = set()
+            nodes.add(0)
+            possible_set = set()
+            added_edges = set()
+            visited = set()
+
+            for edge in nx.edges(G, nbunch=list(nodes)):
+                possible_set.add((edge[1], edge[0]))
+            
+            while len(possible_set) != 0:
+                consideration = choice(list(possible_set))
+                possible_set.remove(consideration)
+                visited.add(consideration)
+
+                start = explainer.edge_index_adj[0, :] == consideration[0]
+                end = explainer.edge_index_adj[1, :] == consideration[1]
+                idx_edge = (start * end).nonzero().item()
+
+                include = pyro.sample(f"m_{idx_edge}", dist.Bernoulli(sample_dists[idx_edge]))
+
+                if include.item() > 0.99:
+                    added_edges.add(consideration)
+                    edge_mask[idx_edge] += 1
+                    nodes.add(consideration[0])
+                    nodes.add(consideration[1])
+                
+                for edge in nx.edges(G, nbunch=list(nodes)):
+                    rewrap = (edge[1], edge[0])
+                    if rewrap not in added_edges and rewrap not in visited:
+                        possible_set.add(rewrap)
+        return edge_mask / 1000
+
+    def ret_probs(self, explainer):
+        return list(map(lambda i: pyro.param(f"m_q_{i}"), range(self.ne)))
 
     def loss_fn(self, model, guide, *args, **kwargs):
         return pyro.infer.Trace_ELBO().differentiable_loss(model, guide, *args)
