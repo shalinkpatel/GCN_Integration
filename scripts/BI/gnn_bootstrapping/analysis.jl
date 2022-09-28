bb589c2b05cff07aab5213fac50300bdd7870a52
Shalin Patel <shalin.kp.patel@gmail.com>
2022-09-28 10:40:58 -0400

    Started final analysis

---
+++ scripts/BI/gnn_bootstrapping/analysis.jl
@@ -0,0 +1,21 @@
+include("load.jl")
+include("noise.jl")
+include("metrics.jl")
+
+using CairoMakie, AlgebraOfGraphics, DataFrames, DataFramesMeta
+CairoMakie.activate!(type="svg")
+
+function eval_params(model :: GNNChain, G :: GNNGraph, λ, ρ)
+    return acc(noisy_graph(G, λ, ρ), model)
+end
+
+base_model = load_model()
+G = load_data()
+
+λs = 1:10
+ρs = 2:10
+ 
+grid = Iterators.product(λs, ρs) |> collect
+results = map(t -> eval_params(base_model, G, t[1], t[2]), grid)
+
+heatmap(results)
--- scripts/BI/gnn_bootstrapping/load.jl
+++ scripts/BI/gnn_bootstrapping/load.jl
@@ -1,4 +1,4 @@
-using Graphs, NPZ, GraphNeuralNetworks
+using Graphs, NPZ, GraphNeuralNetworks, JLD2
 
 function load_data()
     x = npzread("data/x.npy")' |> Array
@@ -8,4 +8,8 @@ function load_data()
     G = SimpleDiGraph(length(y))
     foreach(r -> add_edge!(G, r[1], r[2]), eachrow(g))
     return GNNGraph(G, ndata=(; x = x, y = y))
+end
+
+function load_model()
+    return load("data/base.model", "model")
 end
