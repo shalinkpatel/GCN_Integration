0ec8dbc73236698f653dcfcb69e4a61b31cca41e
Shalin Patel <shalin.kp.patel@gmail.com>
2022-10-11 15:51:44 -0400

    put noisy data into edge list

---
+++ scripts/BI/gnn_bootstrapping/serial.jl
@@ -0,0 +1,14 @@
+include("load.jl")
+include("noise.jl")
+
+G = load_data()
+λ = 2.5
+ρ = 3
+
+Ḡ = noisy_graph(G, λ, ρ)
+
+open("data/noisy.data", "w") do f
+    for (s, d) ∈ zip(Ḡ.graph[1], Ḡ.graph[2])
+        write(f, "$(s),$(d)\n")
+    end
+end;
