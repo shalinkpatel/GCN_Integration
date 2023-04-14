a540c155687ac0d95e0bf83dec0514cfb4489aef
Shalin Patel <shalin.kp.patel@gmail.com>
2023-04-13 22:25:02 -0400

    work?

--- scripts/BI/pyro_model/experiments/tree/deg.py
+++ scripts/BI/pyro_model/experiments/tree/deg.py
@@ -4,7 +4,7 @@ import pandas as pd
 import torch
 
 
-model, X, y, G, _ = get_or_train_model()
+model, X, y, G, gt_grn = get_or_train_model()
 
 df = pd.DataFrame(columns=["sparsity", "acc"])
 for i in range(11):
@@ -19,4 +19,26 @@ for i in range(11):
     acc = correct / y.shape[0]
     df.loc[len(df.index)] = (i / 10, acc)
     clear_masks(model)
+
+set_masks(model, gt_grn.float(), G, False)
+correct = 0
+for n in range(y.shape[0]):
+    log_logits = model(X[:, n:n + 1], G)
+    probs = log_logits.softmax(dim=1)
+    correct += (torch.argmax(probs) == y[n].item()).float().item()
+acc = correct / y.shape[0]
+clear_masks(model)
+print(f"GRN Acc: {acc}")
+
+
+set_masks(model, 1 - gt_grn.float(), G, False)
+correct = 0
+for n in range(y.shape[0]):
+    log_logits = model(X[:, n:n + 1], G)
+    probs = log_logits.softmax(dim=1)
+    correct += (torch.argmax(probs) == y[n].item()).float().item()
+acc = correct / y.shape[0]
+clear_masks(model)
+print(f"Non-GRN Acc: {acc}")
+
 df.to_csv('experiments/tree/deg.csv', index=False)
