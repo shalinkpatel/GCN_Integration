ac240ee7912b4c05400a681bb58c83752cdbfcaa
Shalin Patel <shalin.kp.patel@gmail.com>
2023-04-16 11:45:08 -0400

    added logs

--- scripts/BI/pyro_model/experiments/tree/explainers.py
+++ scripts/BI/pyro_model/experiments/tree/explainers.py
@@ -28,38 +28,6 @@ X = X[:, y == 1]
 y = y[y == 1]
 print(len(y))
 
-print('=' * 20 + " GNN Explainer " + '=' * 20)
-metrics_gnn_exp = [0, 0, 0, 0, 0]
-gnn_explainer = Explainer(
-    model=model,
-    algorithm=GNNExplainer(epochs=500),
-    explanation_type='model',
-    node_mask_type='attributes',
-    edge_mask_type='object',
-    model_config=dict(
-        mode='multiclass_classification',
-        task_level='graph',
-        return_type='log_probs',
-    ),
-)
-final_gnnexp_explanation = torch.zeros_like(gt_grn).float()
-avg_gnnexp_explanation = torch.zeros_like(gt_grn).float()
-for x in range(y.shape[0]):
-    start = time.time()
-    gnn_exp_explanation = gnn_explainer(X[:, x:x + 1], G)
-    print(f"Time for graph {x}: {time.time() - start}")
-    final_gnnexp_explanation = torch.max(final_gnnexp_explanation, gnn_exp_explanation.edge_mask)
-    avg_gnnexp_explanation += gnn_exp_explanation.edge_mask
-    res = groundtruth_metrics(gnn_exp_explanation.edge_mask, gt_grn)
-    metrics_gnn_exp = [m + r for m, r in zip(metrics_gnn_exp, res)]
-metrics_gnn_exp = [m / y.shape[0] for m in metrics_gnn_exp]
-avg_gnnexp_explanation /= y.shape[0]
-print('=' * 20 + " GNNExp Results " + '=' * 20)
-print({n: v for n, v in zip(m_names, metrics_gnn_exp)})
-print({n: v for n, v in zip(m_names, groundtruth_metrics(final_gnnexp_explanation, gt_grn))})
-print({n: v for n, v in zip(m_names, groundtruth_metrics(avg_gnnexp_explanation, gt_grn))})
-save_masks("gnnexp_max_mask", gt_grn, final_gnnexp_explanation, G)
-save_masks("gnnexp_avg_mask", gt_grn, avg_gnnexp_explanation, G)
 
 print('=' * 20 + " DNFG Explainer " + '=' * 20)
 metrics_dnf_grad = [0, 0, 0, 0, 0]
@@ -76,15 +44,12 @@ for x in samples[:int(0.25 * len(samples))]:
     explainer.train(3000, 1e-4)
     print(f"Time for graph {graph}: {time.time() - start}")
     explainer_mask = explainer.edge_mask().detach()
-    explainer.clean()
-    del explainer
-    if torch.isnan(explainer_mask).sum() == explainer_mask.shape[0]:
-        del explainer_mask
-        continue
+    print(f"Positive Accuracy: {(explainer_mask[gt_grn == 1] > 0.5).float().mean()}")
+    print(f"Negative Accuracy: {(explainer_mask[gt_grn == 0] < 0.5).float().mean()}")
+    print(f"Edge Mask: {explainer_mask()}")
     final_dnfgexp_explanation = torch.max(final_dnfgexp_explanation, explainer_mask)
     avg_dnfgexp_explanation += explainer_mask
     res = groundtruth_metrics(explainer_mask, gt_grn)
-    del explainer_mask
     metrics_dnf_grad = [m + r for m, r in zip(metrics_dnf_grad, res)]
     n_samples += 1
 metrics_nf_grad = [m / n_samples for m in metrics_dnf_grad]
@@ -129,3 +94,36 @@ print({n: v for n, v in zip(m_names, groundtruth_metrics(final_betaexp_explanati
 print({n: v for n, v in zip(m_names, groundtruth_metrics(avg_betaexp_explanation, gt_grn))})
 save_masks("betaexp_max_mask", gt_grn, final_betaexp_explanation, G)
 save_masks("betaexp_avg_mask", gt_grn, avg_betaexp_explanation, G)
+
+print('=' * 20 + " GNN Explainer " + '=' * 20)
+metrics_gnn_exp = [0, 0, 0, 0, 0]
+gnn_explainer = Explainer(
+    model=model,
+    algorithm=GNNExplainer(epochs=500),
+    explanation_type='model',
+    node_mask_type='attributes',
+    edge_mask_type='object',
+    model_config=dict(
+        mode='multiclass_classification',
+        task_level='graph',
+        return_type='log_probs',
+    ),
+)
+final_gnnexp_explanation = torch.zeros_like(gt_grn).float()
+avg_gnnexp_explanation = torch.zeros_like(gt_grn).float()
+for x in range(y.shape[0]):
+    start = time.time()
+    gnn_exp_explanation = gnn_explainer(X[:, x:x + 1], G)
+    print(f"Time for graph {x}: {time.time() - start}")
+    final_gnnexp_explanation = torch.max(final_gnnexp_explanation, gnn_exp_explanation.edge_mask)
+    avg_gnnexp_explanation += gnn_exp_explanation.edge_mask
+    res = groundtruth_metrics(gnn_exp_explanation.edge_mask, gt_grn)
+    metrics_gnn_exp = [m + r for m, r in zip(metrics_gnn_exp, res)]
+metrics_gnn_exp = [m / y.shape[0] for m in metrics_gnn_exp]
+avg_gnnexp_explanation /= y.shape[0]
+print('=' * 20 + " GNNExp Results " + '=' * 20)
+print({n: v for n, v in zip(m_names, metrics_gnn_exp)})
+print({n: v for n, v in zip(m_names, groundtruth_metrics(final_gnnexp_explanation, gt_grn))})
+print({n: v for n, v in zip(m_names, groundtruth_metrics(avg_gnnexp_explanation, gt_grn))})
+save_masks("gnnexp_max_mask", gt_grn, final_gnnexp_explanation, G)
+save_masks("gnnexp_avg_mask", gt_grn, avg_gnnexp_explanation, G)
