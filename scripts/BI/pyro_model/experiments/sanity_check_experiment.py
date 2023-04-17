d92af12af70f0880bac271187d269667eada4ce6
Shalin Patel <shalin.kp.patel@gmail.com>
2023-04-17 15:20:35 -0400

    sanity check beta

--- scripts/BI/pyro_model/experiments/sanity_check_experiment.py
+++ scripts/BI/pyro_model/experiments/sanity_check_experiment.py
@@ -1,32 +1,19 @@
-from operator import truth
-import sys
+from model.Experiment import Experiment
+from model.samplers.RandomWalkSampler import RandomWalkSampler
+from model.searchers.GNNExplainerSearcher import GNNExplainerSearcher
+from model.samplers.NFGradSampler import NFGradSampler
+from model.DeterministicExplainer import DeterministicExplainer
+from model.BayesExplainer import BayesExplainer
+from model.samplers.BaseSampler import BaseSampler
+from model.BetaExplainer import BetaExplainer
 
-sys.path.append("/users/spate116/singhlab/GCN_Integration/scripts/BI/pyro_model/model")
-
-from Experiment import Experiment
-from samplers.NFSampler import NFSampler
-from samplers.SpikeSlabSampler import SpikeSlabSampler
-from samplers.RandomWalkSampler import RandomWalkSampler
-from searchers.GNNExplainerSearcher import GNNExplainerSearcher
-from searchers.GreedySearcher import GreedySearcher
-from samplers.BetaGradSampler import BetaGradSampler
-from samplers.NFGradSampler import NFGradSampler
-from samplers.SpikeSlabGradSampler import SpikeSlabGradSampler
-from DeterministicExplainer import DeterministicExplainer
-from BayesExplainer import BayesExplainer
-from MCMCExplainer import MCMCExplainer
-from samplers.BaseSampler import BaseSampler
-from searchers.BaseSearcher import BaseSearcher
-
+from torch_geometric.utils import k_hop_subgraph
 from loguru import logger
 
 import pyro
 import torch
-import torch.nn.functional as F
 from sklearn.metrics import roc_auc_score, accuracy_score, f1_score, average_precision_score
 import numpy as np
-
-from random import shuffle
 
 
 def edge_list_to_edge_index(fl: str) -> torch.Tensor:
@@ -36,12 +23,12 @@ def edge_list_to_edge_index(fl: str) -> torch.Tensor:
     return torch.tensor(list(map(lambda ln: list(map(lambda x: int(x), ln.split(','))), lines))).T
 
 
-def is_noise(experiment : Experiment, edge_index : torch.Tensor) -> torch.Tensor:
+def is_noise(experiment: Experiment, edge_index: torch.Tensor) -> torch.Tensor:
     base_ei = experiment.edge_index
     all_edges = set()
     for i in range(base_ei.shape[1]):
         all_edges.add((base_ei[0, i].item(), base_ei[1, i].item()))
-    
+
     label = []
 
     for i in range(edge_index.shape[1]):
@@ -53,19 +40,19 @@ def is_noise(experiment : Experiment, edge_index : torch.Tensor) -> torch.Tensor
     return torch.tensor(label)
 
 
-def get_exp_sampler(experiment : Experiment, edge_index, sampler, backend, n, **train_hparams) -> torch.Tensor:
+def get_exp_sampler(experiment: Experiment, edge_index, sampler, backend, n, **train_hparams) -> torch.Tensor:
     node_exp = backend(experiment.model, sampler, n, experiment.k, experiment.x, experiment.data.y, edge_index)
     node_exp.train(log=False, **train_hparams)
     return node_exp.edge_mask(), node_exp
 
 
-def get_exp_searcher(experiment : Experiment, edge_index, searcher, backend, n, **train_hparams) -> torch.Tensor:
-        node_exp = backend(experiment.model, searcher, n, experiment.k, experiment.x, experiment.data.y, edge_index)
-        edge_mask = node_exp.edge_mask('.', **train_hparams)
-        return edge_mask, node_exp
+def get_exp_searcher(experiment: Experiment, edge_index, searcher, backend, n, **train_hparams) -> torch.Tensor:
+    node_exp = backend(experiment.model, searcher, n, experiment.k, experiment.x, experiment.data.y, edge_index)
+    edge_mask = node_exp.edge_mask('.', **train_hparams)
+    return edge_mask, node_exp
 
 
-def test_sampler(experiment : Experiment, edge_index : torch.Tensor, labels : torch.Tensor, sampler, backend, name : str, **train_hparams):
+def test_sampler(experiment: Experiment, edge_index: torch.Tensor, labels: torch.Tensor, sampler, backend, name: str, **train_hparams):
     logger.info(f"Testing sampler {name}")
 
     auc = 0
@@ -128,6 +115,68 @@ def test_sampler(experiment : Experiment, edge_index : torch.Tensor, labels : to
     logger.info(f"{name.replace('||', '.')} | FINISHED | avg_f1 {f1 / done}")
 
 
+def test_new_explainer(experiment: Experiment, edge_index: torch.Tensor, labels: torch.Tensor, name: str):
+    logger.info(f"Testing sampler {name}")
+
+    auc = 0
+    acc = 0
+    aupr = 0
+    f1 = 0
+    itr_aucs = []
+    itr_accs = []
+    itr_auprs = []
+    itr_f1s = []
+
+    done = 0
+    nodes = list(range(experiment.x.shape[0]))
+
+    X = experiment.x
+    k = experiment.k
+
+    for n in nodes:
+        try:
+            pyro.clear_param_store()
+            subset, edge_index_adj, mapping, edge_mask_hard = k_hop_subgraph(n, k, edge_index, relabel_nodes=True)
+            X_adj = X[subset]
+            explainer = BetaExplainer(experiment.model, X_adj, edge_index, torch.device('cpu'))
+            explainer.train(20000, 1e-4)
+            edge_mask = explainer.edge_mask()
+
+            labs = labels[edge_mask_hard]
+
+            for i, v in enumerate(labs.cpu().detach().numpy().tolist()):
+                if v == 1:
+                    edge_mask[i] = 1
+
+            itr_auc = roc_auc_score(labs, edge_mask.cpu().detach().numpy(), average="weighted")
+            itr_acc = accuracy_score(labs, np.array(list(map(lambda x: 0 if x <= 0.5 else 1, edge_mask.detach().cpu().numpy()))))
+            itr_aupr = average_precision_score(labs, edge_mask.cpu().detach().numpy(), average="weighted")
+            itr_f1 = f1_score(labs, np.array(list(map(lambda x: 0 if x <= 0.5 else 1, edge_mask.detach().cpu().numpy()))))
+
+            itr_aucs.append(itr_auc)
+            itr_accs.append(itr_acc)
+            itr_auprs.append(itr_aupr)
+            itr_f1s.append(itr_f1)
+
+            auc += itr_auc
+            acc += itr_acc
+            aupr += itr_aupr
+            f1 += itr_f1
+
+            done += 1
+
+            logger.info(f"{name.replace('||', '.')} | {n} | itr_auc {itr_auc}")
+            logger.info(f"{name.replace('||', '.')} | {n} | avg_auc {auc / done}")
+            logger.info(f"{name.replace('||', '.')} | {n} | itr_acc {itr_acc}")
+            logger.info(f"{name.replace('||', '.')} | {n} | avg_acc {acc / done}")
+            logger.info(f"{name.replace('||', '.')} | {n} | itr_aupr {itr_aupr}")
+            logger.info(f"{name.replace('||', '.')} | {n} | avg_aupr {aupr / done}")
+            logger.info(f"{name.replace('||', '.')} | {n} | itr_f1 {itr_f1}")
+            logger.info(f"{name.replace('||', '.')} | {n} | avg_f1 {f1 / done}")
+        except ValueError:
+            logger.info(f"Skipping node {n} because there is only one class present in labels")
+
+
 # --------------- Setting Up Data -----------------
 experiment = Experiment("syn3-full-verifed", "..")
 experiment.train_base_model()
@@ -136,6 +185,11 @@ logger.info("Finished training base model")
 
 noisy_G = edge_list_to_edge_index("/users/spate116/singhlab/GCN_Integration/scripts/BI/pyro_model/data/sanity/noisy.data")
 gr_truth = is_noise(experiment, noisy_G)
+
+# --------------- BetaExplainer --------------------
+test_new_explainer(experiment, noisy_G, gr_truth, "BetaExplainer")
+logger.info("Finished evaluating BetaExplainer on Sanity Check Experiment")
+exit()
 
 # --------------- NFGradSampler -----------------
 nfg_hparams = {
