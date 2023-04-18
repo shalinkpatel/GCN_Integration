ccc2a8f17529ce78d98220b9d9689651d30cd088
Shalin Patel <shalin.kp.patel@gmail.com>
2023-04-18 19:31:14 -0400

    updated test bench

--- scripts/BI/pyro_model/experiments/tree/test_script.py
+++ scripts/BI/pyro_model/experiments/tree/test_script.py
@@ -1,5 +1,6 @@
 from model.DNFGExplainer import DNFGExplainer
 from experiments.tree.tree_model import get_or_train_model
+from torch_geometric.explain.metric.basic import groundtruth_metrics
 import torch
 import numpy as np
 
@@ -10,6 +11,7 @@ explainer.train(3000, 1e-4)
 print(f"Positive Accuracy: {(explainer.edge_mask()[gt_grn == 1] > 0.5).float().mean()}")
 print(f"Negative Accuracy: {(explainer.edge_mask()[gt_grn == 0] < 0.5).float().mean()}")
 print(f"Edge Mask: {explainer.edge_mask()}")
+print(f"Metrics: {groundtruth_metrics(explainer.edge_mask(), gt_grn)}")
 np.save("experiments/tree/example_DNFG_dist_1.npy", explainer.edge_distribution().numpy())
 
 explainer = DNFGExplainer(model, 8, X[:, 601:602], G, torch.device('cpu'))
@@ -17,4 +19,5 @@ explainer.train(3000, 1e-4)
 print(f"Positive Accuracy: {(explainer.edge_mask()[gt_grn == 1] > 0.5).float().mean()}")
 print(f"Negative Accuracy: {(explainer.edge_mask()[gt_grn == 0] < 0.5).float().mean()}")
 print(f"Edge Mask: {explainer.edge_mask()}")
+print(f"Metrics: {groundtruth_metrics(explainer.edge_mask(), gt_grn)}")
 np.save("experiments/tree/example_DNFG_dist_2.npy", explainer.edge_distribution().numpy())
--- scripts/BI/pyro_model/experiments/tree/test_script_beta.py
+++ scripts/BI/pyro_model/experiments/tree/test_script_beta.py
@@ -1,5 +1,6 @@
 from model.BetaExplainer import BetaExplainer
 from experiments.tree.tree_model import get_or_train_model
+from torch_geometric.explain.metric.basic import groundtruth_metrics
 import torch
 import numpy as np
 
@@ -10,6 +11,7 @@ explainer.train(20000, 1e-4)
 print(f"Positive Accuracy: {(explainer.edge_mask()[gt_grn == 1] > 0.5).float().mean()}")
 print(f"Negative Accuracy: {(explainer.edge_mask()[gt_grn == 0] < 0.5).float().mean()}")
 print(f"Edge Mask: {explainer.edge_mask()}")
+print(f"Metrics: {groundtruth_metrics(explainer.edge_mask(), gt_grn)}")
 np.save("experiments/tree/example_beta_dist_1.npy", explainer.edge_distribution().numpy())
 
 explainer = BetaExplainer(model, X[:, 601:602], G, torch.device('cpu'))
@@ -17,4 +19,5 @@ explainer.train(20000, 1e-4)
 print(f"Positive Accuracy: {(explainer.edge_mask()[gt_grn == 1] > 0.5).float().mean()}")
 print(f"Negative Accuracy: {(explainer.edge_mask()[gt_grn == 0] < 0.5).float().mean()}")
 print(f"Edge Mask: {explainer.edge_mask()}")
+print(f"Metrics: {groundtruth_metrics(explainer.edge_mask(), gt_grn)}")
 np.save("experiments/tree/example_beta_dist_2.npy", explainer.edge_distribution().numpy())
