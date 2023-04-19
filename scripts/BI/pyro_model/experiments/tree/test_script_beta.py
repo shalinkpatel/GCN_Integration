df812ac5eab0a6f2652cb18c84e33412f6ea7608
shalinkpatel <shalin_patel@brown.edu>
2023-04-18 20:09:12 -0400

    pushing back changes

--- scripts/BI/pyro_model/experiments/tree/test_script_beta.py
+++ scripts/BI/pyro_model/experiments/tree/test_script_beta.py
@@ -21,3 +21,11 @@ print(f"Negative Accuracy: {(explainer.edge_mask()[gt_grn == 0] < 0.5).float().m
 print(f"Edge Mask: {explainer.edge_mask()}")
 print(f"Metrics: {groundtruth_metrics(explainer.edge_mask(), gt_grn)}")
 np.save("experiments/tree/example_beta_dist_2.npy", explainer.edge_distribution().numpy())
+
+explainer = BetaExplainer(model, X[:, 602:603], G, torch.device('cpu'))
+explainer.train(20000, 1e-4)
+print(f"Positive Accuracy: {(explainer.edge_mask()[gt_grn == 1] > 0.5).float().mean()}")
+print(f"Negative Accuracy: {(explainer.edge_mask()[gt_grn == 0] < 0.5).float().mean()}")
+print(f"Edge Mask: {explainer.edge_mask()}")
+print(f"Metrics: {groundtruth_metrics(explainer.edge_mask(), gt_grn)}")
+np.save("experiments/tree/example_beta_dist_2.npy", explainer.edge_distribution().numpy())
